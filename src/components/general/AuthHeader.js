c358f1119770e7166e4aa192843c1dad78e72030
annak <annakachmasheva@gmail.com>
2024-05-20 16:59:37 +0200

    added logo image

--- src/components/general/AuthHeader.js
+++ src/components/general/AuthHeader.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from '../../assets/small-logo.png';
+import logo from '.././../assets/small_logo.png';
 import styles from './AuthHeader.module.scss';
 import {useNavigate} from "react-router-dom";
 
